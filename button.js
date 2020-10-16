f8073ab07c8100f716970bdec0290f7a81c2121b
Tim Greller <timlg07@gmail.com>
2020-10-16 16:23:21 +0200

    Minor code improvements:
    - uniform quotes
    - added doctype declaration
    - added explicit html element

--- button.js
+++ button.js
@@ -6,16 +6,16 @@ function createRipple(event) {
     const radius = diameter / 2;
 
     circle.style.width = circle.style.height = diameter + "px";
-    circle.style.left = (event.clientX - button.offsetLeft - radius) + "px";
-    circle.style.top = (event.clientY - button.offsetTop - radius) + "px";
+    circle.style.left  = (event.clientX - button.offsetLeft - radius) + "px";
+    circle.style.top   = (event.clientY - button.offsetTop  - radius) + "px";
     circle.classList.add("ripple");
 
-    circle.addEventListener('animationend', button.removeChild.bind(button, circle));
+    circle.addEventListener("animationend", button.removeChild.bind(button, circle));
 
     button.appendChild(circle);
 }
 
-addEventListener('load', () => {
+addEventListener("load", () => {
     const buttons = document.getElementsByClassName("ripple-button");
     for (const button of buttons) {
         button.addEventListener("click", createRipple);
